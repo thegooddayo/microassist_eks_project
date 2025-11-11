4f4837e9ac058b75d6163d2d26886f206a1fc6a3
Adedayo Taiwo <thegooddayo@yahoo.com>
2025-11-11 15:19:45 -0500

    upgrade

--- envs/stage/backend.tf
+++ envs/stage/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "eks-upgrade-demo-terraform-state"
+    bucket         = "dayo-eks-upgrade-demo-terraform-state"
     key            = "stage/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
